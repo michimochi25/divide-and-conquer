cc78cf5f6fd1798a3a2d109f2c6ee93c0fc62270
Giselle Angela Hanjaya Putri <z5491414@ad.unsw.edu.au>
2025-07-22 19:11:15 +1000

    fix mobile landing page

--- frontend/src/pages/LandingPage.tsx
+++ frontend/src/pages/LandingPage.tsx
@@ -8,7 +8,7 @@ const LandingPage = () => {
     <div className="flex flex-col items-center justify-center h-screen w-screen text-xl gap-2">
       <header className="flex flex-col items-center justify-center">
         <h1>Welcome to</h1>
-        <h1 className="font-bold">Divide and Conquer</h1>
+        <h1 className="font-bold text-center">Divide and Conquer</h1>
       </header>
       <Button text="START" onClick={() => navigate("/signin")} />
     </div>
